2912961d25764f8cbd2fd6756469c7f5af90d3ee
Buvude <90856761+Buvude@users.noreply.github.com>
2023-04-25 16:01:10 -0400

    updated the debug room a bit more

--- Assets/Scripts/Panel.cs
+++ Assets/Scripts/Panel.cs
@@ -8,7 +8,7 @@ public class Panel : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.FindGameObjectWithTag("Game Manager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
