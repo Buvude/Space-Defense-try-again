3b7043f17b5b65bb54e0494c4dd82c4bef7e4983
Buvude <90856761+Buvude@users.noreply.github.com>
2023-05-09 16:10:10 -0400

    frixxed the scripts

--- Assets/Scripts/Panel.cs
+++ Assets/Scripts/Panel.cs
@@ -4,23 +4,35 @@ using UnityEngine;
 
 public class Panel : MonoBehaviour
 {
+    public Material thisMaterial;
+    public bool broken = false;
     public GameManager gameManager;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+
         gameManager = GameObject.FindGameObjectWithTag("Game Manager").GetComponent<GameManager>();
+        gameManager.panelsInScene.Add(this);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (broken)
+        {
+            thisMaterial.color = Color.red;
+        }
+        else
+        {
+            thisMaterial.color = Color.green;
+        }
     }
 
     public void Repair()
     {
-        if (gameManager.isShipDamaged == true)
+        if (gameManager.isShipDamaged == true && broken)
         {
+            broken = false;
             gameManager.shipStatusText.text = "Ship Status:\n Ship has been repaired";
             gameManager.isShipDamaged = false;
             StartCoroutine(gameManager.BreakShip());
